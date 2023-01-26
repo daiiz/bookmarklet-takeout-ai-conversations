627143b9f2e446d5f311c37bc7c2818a0cabb013
daiiz <da.iz.mg@gmail.com>
2023-01-26 14:49:51 +0900

    a

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -eu
+
+closure-compiler --js index.js --js_output_file out/bookmarklet.js
+head="javascript:(function(){"
+tail="})()"
+
+body=$(cat out/bookmarklet.js)
+
+echo "${head}${body}${tail}" > out/bookmarklet.js
---
+++ index.js
@@ -0,0 +1,6 @@
+const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => document.querySelectorAll(selector);
+
+const getChatTitle = () => {};
+
+const getChatTexts = () => {};
