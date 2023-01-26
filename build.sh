8d5fa1b97362dd21a38416c14bfbc2c26cdcbe4d
daiiz <da.iz.mg@gmail.com>
2023-01-26 16:33:03 +0900

    fix

--- build.sh
+++ build.sh
@@ -2,9 +2,5 @@
 set -eu
 
 closure-compiler --js index.js --js_output_file out/body.js
-# head="javascript:(function(){"
-# tail="})()"
-# body=$(cat index.js)
-# echo "${head}${body}${tail}" > out/bookmarklet.js
 
 cat out/head.js out/body.js out/tail.js > out/bookmarklet.js
