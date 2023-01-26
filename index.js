a1183e8adf2f4ec3a2fb6d062a46824c92047356
daiiz <da.iz.mg@gmail.com>
2023-01-26 15:07:35 +0900

    foo

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
+const yourName = "daiiz"; // Please change to your name
+
 const $ = (selector) => document.querySelector(selector);
 const $$ = (selector) => document.querySelectorAll(selector);
 
-const getChatTitle = () => {};
+const getChatTitle = () => {
+  return $("title").textContent;
+};
 
-const getChatTexts = () => {};
+const getChatContents = () => {
+  const res = [];
+  const textElems = $$(".text-base");
+  for (let i = 0; i < textElems.length; i++) {
+    const textElem = textElems[i];
+    const text = textElem.textContent;
+    const imgElem = textElem.querySelector("img");
+    const svgElem = textElem.querySelector("svg");
+
+    let speaker = yourName;
+    if (imgElem) {
+      if (!yourName) {
+        speaker = imgElem.getAttribute("alt") || "unknown";
+      }
+    } else if (svgElem) {
+      speaker = "chatgpt";
+    }
+    const icon = `[${speaker.replace(/\s/g, "_")}.icon]`;
+
+    res.push([icon, text]);
+  }
+  return res;
+};
+
+const createScrapboxLines = () => {
+  const res = [];
+  const title = getChatTitle();
+  const contents = getChatContents();
+  res.push(`[${title}]`);
+  res.push(...contents.map(([icon, text]) => `\t${icon} ${text}`));
+  return res;
+};
