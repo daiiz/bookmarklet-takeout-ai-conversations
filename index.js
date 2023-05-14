908d32f1d14eaead4985b3b8a49f2f71fbd5ab5c
daiiz <da.iz.mg@gmail.com>
2023-05-14 20:25:45 +0900

    f

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const yourName = ""; // Please change to your name
 const chatgptName = "chatgpt";
 const hashtagLine = "#ChatGPT日記"; // Please change to your favaorite hashtags
 
-const getNextjsProps = () => {
+const getNextjsPageProps = () => {
   const nextJsScript = document.querySelector("script#__NEXT_DATA__");
   const data = JSON.parse(nextJsScript?.textContent || "{}");
   return data.props?.pageProps || {};
@@ -44,7 +44,7 @@ const getChatContents = ({ user }) => {
 };
 
 const createScrapboxLines = () => {
-  const props = getNextjsProps();
+  const props = getNextjsPageProps();
   const res = [];
   const title = getChatTitle();
   const contents = getChatContents(props);
