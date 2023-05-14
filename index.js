ec56c462684d459da89791ee1eda10cea38ae013
daiiz <da.iz.mg@gmail.com>
2023-05-14 20:21:11 +0900

    fix

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const yourName = ""; // Please change to your name
 const chatgptName = "chatgpt";
 const hashtagLine = "#ChatGPT日記"; // Please change to your favaorite hashtags
 
+const getNextjsProps = () => {
+  const nextJsScript = document.querySelector("script#__NEXT_DATA__");
+  const data = JSON.parse(nextJsScript?.textContent || "{}");
+  return data.props?.pageProps || {};
+};
+
 const getChatTitle = () => {
   return document.querySelector("title").textContent;
 };
 
-const getChatContents = () => {
+const getChatContents = ({ user }) => {
   const res = [];
   const textElems = document.querySelectorAll("div.text-base");
   for (let i = 0; i < textElems.length; i++) {
@@ -23,7 +29,7 @@ const getChatContents = () => {
       speaker = chatgptName;
     }
     if (imgElem && !yourName) {
-      speaker = imgElem.alt || "me";
+      speaker = user?.name || "me";
     }
     const icon = `[${speaker.replace(/\s/g, "_")}.icon]`;
 
@@ -38,9 +44,10 @@ const getChatContents = () => {
 };
 
 const createScrapboxLines = () => {
+  const props = getNextjsProps();
   const res = [];
   const title = getChatTitle();
-  const contents = getChatContents();
+  const contents = getChatContents(props);
   res.push(title);
   res.push(new Date().toLocaleDateString(), "");
   res.push(...contents);
